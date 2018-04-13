27dacf91dfef7bbfde9949b85f98a41d671effb1
zsl <zslngu@163.com>
2018-04-13 21:50:55 +0800

    还是有问题
    自己没想清楚什么是partition
    partition左边的元素都小于等于它
    partition右边的元素都大于它
    
    一定要弄清楚定义和什么是快排

--- quicksort_2.java
+++ quicksort_2.java
@@ -2,7 +2,7 @@
 //quicsort——1 不能处理有多个相同元素
 import java.io.*;
 import java.util.*;
-public class quicksort{
+public class quicksort_2{
 
 	public <T extends Comparable<T>> void quiksort(T [] array){
 
@@ -10,7 +10,7 @@ public class quicksort{
 	}
 
 	public <T extends Comparable<T>> void sort(T [] array,int left,int right){
-		if(left<right-1){
+		if(left<right){
 			System.out.println("division...");
 			System.out.println(left+"	"+right);
 
@@ -19,30 +19,38 @@ public class quicksort{
 			System.out.println("选择第"+pivot+"个");
 			Scanner scan=new Scanner(System.in);
 			String temp=scan.nextLine();
-
-			sort(array,left,pivot);
+			//if(pivot!=right){  //gen ju dai ma luo ji pivot==right shi zuo bian yi pai hao xu
+				//chong fu diao yong hui si xun huan
+			sort(array,left,pivot);//}
 			sort(array,pivot+1,right);
 		}
 	}
 
 	public <T extends Comparable<T>> int partition(T [] array,int left,int right){
 		//select the left element as pivot
+		System.out.println("enter the partition...");
 		T pivot=array[left];
 		int l=left;
-		right+=1;  //keep the consistence of data
+		//right+=1;  //keep the consistence of data
 		while(left<right){
 
 			//问题在于不能上来就直接加
 			//假设  900 5 10
 			//      900 与 10交换后 仍需10 与比较 若直接索引加之后 就会出错
-			while(((left++)>=0)&&(left<right-1)&&(array[left].compareTo(pivot)<=0)){
-				//System.out.println(left);
+
+			//条件的判断有问题，一方面left要小于right 另一方面left的位置不能大于pivot
+			//自己这样判断无法去除left所在位置大于pivot的情况
+			//整体的规划有问题 
+
+			while((left<right)&&(array[left].compareTo(pivot)<=0)){
+				left++;
 			}
-			while(((right--)>=0)&&(right>=left)&&(array[right].compareTo(pivot)>0)){
-				//System.out.println(right);
+			while((right>left)&&(array[right].compareTo(pivot)>0)){
+				right--;
 			}
 			
-			if(left<right){
+			if(left<=right){
+				System.out.println("移动后..."+left+"	"+right);
 				System.out.println(array[left]+"与"+array[right]+"交换");
 				T temp=array[left];
 				array[left]=array[right];
@@ -55,22 +63,23 @@ public class quicksort{
 			}
 
 		}
-
-		array[l]=array[--left];
+		//想一想自己这么一套算法过后 pivot左边 右边元素各是什么样的
+		array[l]=array[left];
 		array[left]=pivot;
+		System.out.println(left);
 		return left;
 	}
 
 	public static void main(String [] args){
 
-		int test[]={1,3,4,9,4,5,3,33,900,4,3,10};
-
+		//int test[]={1,3,4,9,4,5,3,33,900,4,3,10};
+		int test[]={5,4,3,1,2};
 		Integer t[]=new Integer[test.length];
 
 		for(int i=0;i<test.length;i++){
 			t[i]=test[i];
 		}
-		quicksort q=new quicksort();
+		quicksort_2 q=new quicksort_2();
 
 		q.quiksort(t);
 		System.out.println("from main--------");
